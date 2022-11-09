dcbac718741c407bf4a4a8c09ec7610bcfefccc5
Nick Wylynko <nick@wylynko.com>
2022-11-09 19:03:35 +0800

    Fix for page tab order

--- app/LinkItem.tsx
+++ app/LinkItem.tsx
@@ -8,10 +8,13 @@ interface LinkItemProps {
   icon: IconType;
 }
 
+// setting tab index to negative 1 so its not tabbable too
+// as the user can already tab to the <a> element
+
 export const LinkItem = ({ label, url, icon: Icon }: LinkItemProps) => {
   return (
     <ExternalLink href={url} className={styles.link}>
-      <button className={styles.button}>
+      <button className={styles.button} tabIndex={-1}>
         <Icon size={24} />
         <span>{label}</span>
         <div />
