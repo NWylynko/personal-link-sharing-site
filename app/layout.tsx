701398554a274b3cafe52d5497ab300fbb9d2a27
Nick Wylynko <nick@wylynko.com>
2022-11-09 18:02:43 +0800

    Apparently this is the proper link idk

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <title>Nick Wylynko</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
       </head>
       <body>
         {children}
