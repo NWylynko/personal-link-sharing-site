039faf975bdeee9634f3c321c6f2b221bfd599c4
Nick Wylynko <nick@wylynko.com>
2022-11-09 16:53:51 +0800

    Add some style to text and image

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import ProfilePic from "../assets/profile.png"
+import styles from "./styles.module.css"
 
 
 export default function HomePage() {
   return (
     <div>
-      <Image src={ProfilePic} alt="Profile Picture" height={96} width={96} />
-      <h1>Nick Wylynko</h1>
-      <h2>I build projects with Typescript</h2>
+      <Image className={styles.image} src={ProfilePic} alt="Profile Picture" height={96} width={96} />
+      <h1 className={styles.title}>Nick Wylynko</h1>
+      <h2 className={styles.blurb}>I build projects with Typescript</h2>
     </div>
   )
 }
