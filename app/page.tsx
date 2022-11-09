1d7788d96f954adc7c313feed372e7573b09d7ea
Nick Wylynko <nick@wylynko.com>
2022-11-09 16:52:11 +0800

    Add profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import ProfilePic from "../assets/profile.png"
 
 
 export default function HomePage() {
   return (
-    <span>Letss gooo</span>
+    <div>
+      <Image src={ProfilePic} alt="Profile Picture" height={96} width={96} />
+      <h1>Nick Wylynko</h1>
+      <h2>I build projects with Typescript</h2>
+    </div>
   )
 }
