359e0da9fdfb50a799925229bf66134eb0efd02d
Nick Wylynko <nick@wylynko.com>
2022-11-10 19:02:10 +0800

    remove unnecessary link to github

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,9 @@ export default function HomePage() {
 
       <Repos />
       <span className={styles.sub}><ExternalLink href="https://github.com/NWylynko/personal-link-sharing-site" className={styles.link}>Fork the repo</ExternalLink> to make you own version.</span>
-      <span className={styles.sub}>Built with <ExternalLink href="https://nextjs.org/" className={styles.link}>Next.js</ExternalLink>, deployed to <ExternalLink href="https://vercel.com/" className={styles.link}>Vercel</ExternalLink> from <ExternalLink href="https://github.com/" className={styles.link}>Github</ExternalLink>.</span>
+      <span className={styles.sub}>Built with <ExternalLink href="https://nextjs.org/" className={styles.link}>Next.js</ExternalLink>, deployed to <ExternalLink href="https://vercel.com/" className={styles.link}>Vercel</ExternalLink>.</span>
     </div>
   )
 }
+
+// unnecessary 
