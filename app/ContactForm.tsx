79d679247547fd3c81e5e0f11613d558fffdcc05
Nick Wylynko <nick@wylynko.com>
2022-11-10 16:04:44 +0800

    Add contact form

---
+++ app/ContactForm.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { z } from "zod"
+import { useForm, ValidationError } from '@formspree/react';
+import { useId } from "react";
+import styles from "./ContactForm.module.css"
+import { TbCheck } from "@react-icons/all-files/tb/TbCheck"
+
+const envSchema = z.object({
+  NEXT_PUBLIC_FORMSPREE_TOKEN: z.string()
+})
+
+const env = envSchema.parse({
+
+  // gotta be like this for the transform to work correctly
+  NEXT_PUBLIC_FORMSPREE_TOKEN: process.env.NEXT_PUBLIC_FORMSPREE_TOKEN
+})
+
+export const ContactForm = () => {
+
+  const [state, handleSubmit] = useForm(env.NEXT_PUBLIC_FORMSPREE_TOKEN);
+
+  const emailId = useId()
+  const messageId = useId()
+
+  if (state.succeeded) {
+    return (
+      <div className={styles.successContainer}>
+        <TbCheck size={24} color="#68c968" />
+        <span>Thank you, I will be in contact very soon.</span>
+      </div>
+    )
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+
+      <div className={styles.formArea}>
+        <label htmlFor={emailId} className={styles.label}>Email</label>
+        <input id={emailId} type="email" name="email" className={styles.input} />
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+          className={styles.error}
+        />
+      </div>
+
+      <div className={styles.formArea}>
+        <label htmlFor={messageId} className={styles.label}>Message</label>
+        <textarea id={messageId} name="message" className={styles.textarea} />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+          className={styles.error}
+        />
+      </div>
+
+      <button type="submit" disabled={state.submitting} className={styles.submit}>
+        Submit
+      </button>
+    </form>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { TbBrandLinkedin } from "@react-icons/all-files/tb/TbBrandLinkedin"
 import { TbCpu } from "@react-icons/all-files/tb/TbCpu"
 import { ExternalLink } from "../components/ExternalLink";
 import { Repos } from "./Repos";
+import { ContactForm } from "./ContactForm";
 
 
 export default function HomePage() {
@@ -30,6 +31,11 @@ export default function HomePage() {
         <LinkItem label="Linkedin" url="https://www.linkedin.com/in/nick-wylynko/" icon={TbBrandLinkedin} />
         <LinkItem label="NPM" url="https://www.npmjs.com/~nwylynko" icon={TbCpu} />
       </List>
+      <h1 style={{ color: "white" }}>Contact Me</h1>
+      <details className={styles.contactDetails}>
+        <summary className={styles.contactSummary}>Click here for contact form 📝</summary>
+        <ContactForm />
+      </details>
       <Repos />
       <span className={styles.sub}><ExternalLink href="https://github.com/NWylynko/personal-link-sharing-site" className={styles.link}>Fork the repo</ExternalLink> to make you own version.</span>
       <span className={styles.sub}>Built with <ExternalLink href="https://nextjs.org/" className={styles.link}>Next.js</ExternalLink>, deployed to <ExternalLink href="https://vercel.com/" className={styles.link}>Vercel</ExternalLink> from <ExternalLink href="https://github.com/" className={styles.link}>Github</ExternalLink>.</span>
