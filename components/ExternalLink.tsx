0353ce3f8a732ff580e9687bd95cbcab72d0c020
Nick Wylynko <nick@wylynko.com>
2023-04-05 11:07:45 +0800

    allow referrer (for analytics)

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -4,5 +4,5 @@ import Link from "next/link"
 type ExternalLink = React.ComponentProps<typeof Link>
 
 export const ExternalLink = (props: ExternalLink) => {
-  return <Link {...props} target="_blank" rel="noopener noreferrer" className={clsx("underline", props.className)} />
+  return <Link {...props} target="_blank" rel="noopener" className={clsx("underline", props.className)} />
 }
